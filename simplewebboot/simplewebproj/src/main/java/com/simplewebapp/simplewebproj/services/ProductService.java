bb9093bc8de963d5435ad1563fede04623523151
Darshan <darshanpatel2923@gmail.com>
2024-08-22 22:49:22 +0530

    added post and get operations for our list(dummy data)

--- simplewebboot/simplewebproj/src/main/java/com/simplewebapp/simplewebproj/services/ProductService.java
+++ simplewebboot/simplewebproj/src/main/java/com/simplewebapp/simplewebproj/services/ProductService.java
@@ -10,9 +10,20 @@ import com.simplewebapp.simplewebproj.models.Product;
 
 @Service
 public class ProductService {
-    List<Product> products = Arrays.asList(new Product(101, "Iphone", 50000), new Product(102, "Camera", 12000));
+    List<Product> products = new ArrayList<>(
+            Arrays.asList(new Product(101, "Iphone", 50000), new Product(102, "Camera", 12000)));
 
     public List<Product> getProducts() {
         return products;
+    }
+
+    public Product getProdById(int prodId) {
+        return products.stream()
+                .filter(p -> p.getProdId() == prodId)
+                .findFirst().get();
+    }
+
+    public void addProduct(Product prod) {
+        products.add(prod);
     }
 }
