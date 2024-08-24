67cc9e0d36a88187a3c2a4fad769bbb0a38febfc
Darshan <darshanpatel2923@gmail.com>
2024-08-24 21:14:45 +0530

    started with spring security

---
+++ springsecurity/springsecurity/src/main/java/com/springsecurity/springsecurity/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.springsecurity.springsecurity.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/")
+    public String greet() {
+        return "Hello World";
+    }
+
+}
