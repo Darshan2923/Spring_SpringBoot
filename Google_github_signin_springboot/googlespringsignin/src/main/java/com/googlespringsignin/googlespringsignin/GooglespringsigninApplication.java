cdb67992d9ceeaf755b186d0a00d6a56ff61c9ae
Darshan <darshanpatel2923@gmail.com>
2024-08-25 18:45:33 +0530

    Added Oauth2 login from google and github in another app to the projects

---
+++ Google_github_signin_springboot/googlespringsignin/src/main/java/com/googlespringsignin/googlespringsignin/GooglespringsigninApplication.java
@@ -0,0 +1,13 @@
+package com.googlespringsignin.googlespringsignin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglespringsigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglespringsigninApplication.class, args);
+	}
+
+}
---
+++ Google_github_signin_springboot/googlespringsignin/src/test/java/com/googlespringsignin/googlespringsignin/GooglespringsigninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googlespringsignin.googlespringsignin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglespringsigninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
