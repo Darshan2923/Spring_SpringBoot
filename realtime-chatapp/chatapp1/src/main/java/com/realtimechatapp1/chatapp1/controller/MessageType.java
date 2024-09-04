48144b893ef72fe6c9da4bb39cb7e4d63fc57ecb
Darshan <darshanpatel2923@gmail.com>
2024-09-04 22:09:19 +0530

    Created a realtime chatapp fully working and having feature of realtime chatting

---
+++ realtime-chatapp/chatapp1/src/main/java/com/realtimechatapp1/chatapp1/controller/MessageType.java
@@ -0,0 +1,7 @@
+package com.realtimechatapp1.chatapp1.controller;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ realtime-chatapp/chatapp1/src/test/java/com/realtimechatapp1/chatapp1/Chatapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtimechatapp1.chatapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chatapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
