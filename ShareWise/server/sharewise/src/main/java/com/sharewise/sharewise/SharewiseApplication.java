a1906464dca73b3df14c3be5eeb9a4029c25318a
Darshan <darshanpatel2923@gmail.com>
2024-09-29 22:02:06 +0530

    started a new book/notes social network for college studentshgh

---
+++ ShareWise/server/sharewise/src/main/java/com/sharewise/sharewise/SharewiseApplication.java
@@ -0,0 +1,13 @@
+package com.sharewise.sharewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharewiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharewiseApplication.class, args);
+	}
+
+}
---
+++ ShareWise/server/sharewise/src/test/java/com/sharewise/sharewise/SharewiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharewise.sharewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharewiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
