8372e7b413feb0a1d7ad1973f9c9968bdcb59cfb
Darshan <darshanpatel2923@gmail.com>
2024-09-30 21:32:59 +0530

    Created User and Role with Token models with proper extension of authorization. Will next create Security module for the project

--- ShareWise/server/sharewise/src/main/java/com/sharewise/sharewise/SharewiseApplication.java
+++ ShareWise/server/sharewise/src/main/java/com/sharewise/sharewise/SharewiseApplication.java
@@ -2,8 +2,10 @@ package com.sharewise.sharewise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SharewiseApplication {
 
 	public static void main(String[] args) {
---
+++ ShareWise/server/sharewise/src/main/java/com/sharewise/sharewise/user/TokenRepo.java
@@ -0,0 +1,11 @@
+package com.sharewise.sharewise.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepo extends JpaRepository<Token, Integer> {
+
+    Optional<Token> findByToken(String token);
+
+}
