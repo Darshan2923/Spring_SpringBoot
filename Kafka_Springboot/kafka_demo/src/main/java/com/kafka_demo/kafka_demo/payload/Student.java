eb1053c1e2ea167c39174901d971c985b00bc319
Darshan <darshanpatel2923@gmail.com>
2024-10-23 22:51:18 +0530

    Created json serializer and deserializer and created payload for our json format

---
+++ Kafka_Springboot/kafka_demo/src/main/java/com/kafka_demo/kafka_demo/payload/Student.java
@@ -0,0 +1,13 @@
+package com.kafka_demo.kafka_demo.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Student {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+}
