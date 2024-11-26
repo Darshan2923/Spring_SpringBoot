78c01696115afd78f2b2df35ae33112870ca4144
Darshan <darshanpatel2923@gmail.com>
2024-11-26 18:59:54 +0530

    Added config for producer,consumer and topic in kafka

--- Wussup/wussup/src/main/java/com/wussup/wussup/configuration/KafkaConsumerConfig.java
+++ Wussup/wussup/src/main/java/com/wussup/wussup/configuration/KafkaConsumerConfig.java
@@ -1,5 +1,48 @@
 package com.wussup.wussup.configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import com.wussup.wussup.model.Message;
+
+@EnableKafka
+@Configuration
 public class KafkaConsumerConfig {
 
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory(String groupId) {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, new JsonDeserializer<>(Message.class).getClass());
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.wussup.wussup.model");
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(String groupId) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory(groupId));
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> mobileContainerFactory() {
+        return kafkaListenerContainerFactory("mobile");
+    }
 }
--- Wussup/wussup/src/main/java/com/wussup/wussup/configuration/KafkaTopicConfig.java
+++ Wussup/wussup/src/main/java/com/wussup/wussup/configuration/KafkaTopicConfig.java
@@ -1,5 +1,36 @@
 package com.wussup.wussup.configuration;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaTopicConfig {
 
+    /*
+     * @Value(value="${spring.kafka.bootstrap-servers}")
+     * private String bootstrapAddress;
+     */
+    @Value(value = "${message.topic.private.name}")
+    private String privateTopicName;
+
+    /*
+     * @Bean
+     * public KafkaAdmin kafkaAdmin(){
+     * Map<String,Object> configs=new HashMap<>();
+     * configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapAddress);
+     * return new KafkaAdmin(configs);
+     * }
+     */
+
+    @Bean
+    public NewTopic privateTopic() {
+        return TopicBuilder.name(privateTopicName)
+                .partitions(10)
+                .replicas(1)
+                .compact()
+                .build();
+    }
 }
