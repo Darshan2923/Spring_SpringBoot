639e7e10aac27a59926411c79af026ed0f2beeea
Darshan <darshanpatel2923@gmail.com>
2024-11-28 11:17:44 +0530

    Done with sending chunks of data to the frontend based on user defined bytes and not the whole video is downloaded

--- WizStream/wizstream-client/src/App.jsx
+++ WizStream/wizstream-client/src/App.jsx
@@ -16,9 +16,28 @@ const App = () => {
 
         <div>
           <h1 className='text-white'>Playing video</h1>
-          <video style={{ width: 500, height: 500 }} src={`http://localhost:8080/api/v1/videos/stream/${videoId}/`} controls></video>
-        </div>
+          {/* <video style={{ width: 500, height: 500 }} src={`http://localhost:8080/api/v1/videos/stream/${videoId}/`} controls></video>
+         */}
+          <video
+            id="my-video"
+            class="video-js"
+            controls
+            preload="auto"
+            width="640"
+            data-setup="{}"
+          >
+            <source src={`http:localhost:8080/api/v1/videos/stream/range/${videoId}`} type="video/mp4" />
 
+            <p class="vjs-no-js">
+              To view this video please enable JavaScript, and consider upgrading to a
+              web browser that
+              <a href="https://videojs.com/html5-video-support/" target="_blank"
+              >supports HTML5 video</a
+              >
+            </p>
+          </video>
+
+        </div>
         <VideoUpload />
       </div>
     </>
--- WizStream/wizstream/src/main/java/com/stream/app/wizstream/controller/VideoController.java
+++ WizStream/wizstream/src/main/java/com/stream/app/wizstream/controller/VideoController.java
@@ -1,10 +1,18 @@
 package com.stream.app.wizstream.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.http.HttpHeaders;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.core.io.ByteArrayResource;
 // import org.apache.catalina.connector.Response;
 import org.springframework.core.io.FileSystemResource;
+// import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 // import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +29,7 @@ import org.springframework.core.io.Resource;
 
 import com.stream.app.wizstream.model.Video;
 import com.stream.app.wizstream.services.VideoService;
+import com.stream.app.wizstream.utils.AppConstants;
 import com.stream.app.wizstream.utils.CustomMessage;
 
 @RestController
@@ -88,6 +98,96 @@ public class VideoController {
             contentType = "application/octet-stream";
         }
         return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
+
+    }
+
+    // stream video in chunks
+    @GetMapping("/stream/range/{videoId}")
+    public ResponseEntity<Resource> streamVideoRange(
+            @PathVariable String videoId,
+            @RequestHeader(value = "Range", required = false) String range) {
+        System.out.println(range);
+
+        Video video = videoService.getVideo(videoId);
+        Path path = Paths.get(video.getFilePath());
+
+        Resource resource = new FileSystemResource(path);
+
+        String contentType = video.getContentType();
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        // file length
+        long fileLength = path.toFile().length();
+
+        if (range == null) {
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+                    .body(resource);
+        }
+
+        // calculating start and end range
+        long rangeStart;
+        long rangeEnd;
+
+        String[] ranges = range.replace("bytes=", "").split("-");
+        rangeStart = Long.parseLong(ranges[0]);
+
+        rangeEnd = rangeStart + AppConstants.CHUNK_SIZE - 1;
+
+        if (rangeEnd >= fileLength) {
+            rangeEnd = fileLength - 1;
+        }
+
+        // based on browser range of a video
+        // if (range.length() > 1) {
+        // rangeEnd = Long.parseLong(ranges[1]);
+        // } else {
+        // rangeEnd = fileLength - 1;
+        // }
+
+        // if (rangeEnd > fileLength - 1) {
+        // rangeEnd = fileLength - 1;
+        // }
+
+        System.out.println("range start: " + rangeStart);
+        System.out.println("range end: " + rangeEnd);
+        InputStream inputStream;
+        try {
+
+            inputStream = Files.newInputStream(path);
+            inputStream.skip(rangeStart);
+            // inputStream.close();
+
+            long contentLength = rangeEnd - rangeStart + 1;
+
+            byte[] data = new byte[(int) contentLength];
+            int read = inputStream.read(data, 0, data.length);
+            System.out.println("Read number of bytes: " + read);
+
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("Content-range", "bytes " + rangeStart + "-" + rangeEnd + "/" + fileLength);
+            httpHeaders.add("Cache-control", "no-cache, no-store, must-revalidate");
+            httpHeaders.add("Pragma", "no-cache");
+            httpHeaders.add("Expires", "0");
+            httpHeaders.add("X-Content-Type_Options", "nosniff");
+
+            httpHeaders.setContentLength(contentLength);
+
+            inputStream.close();
+            return ResponseEntity
+                    .status(HttpStatus.PARTIAL_CONTENT)
+                    .headers(httpHeaders)
+                    .contentType(MediaType.parseMediaType(contentType))
+                    // .body(new InputStreamResource(inputStream));
+                    .body(new ByteArrayResource(data));
+
+        } catch (IOException e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
 
     }
 }
