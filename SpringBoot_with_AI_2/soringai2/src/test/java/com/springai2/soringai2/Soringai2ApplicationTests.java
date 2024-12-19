7f2c1a11b506034c904e5091310ddd16797bb051
Darshan <darshanpatel2923@gmail.com>
2024-12-19 18:09:54 +0530

    Started with spring ai toh work on the chatbot projects of future

---
+++ SpringBoot_with_AI_2/soringai2/src/test/java/com/springai2/soringai2/Soringai2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springai2.soringai2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soringai2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
