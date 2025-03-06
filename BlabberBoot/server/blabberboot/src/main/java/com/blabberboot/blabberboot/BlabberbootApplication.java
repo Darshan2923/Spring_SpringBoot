faef367051ea0bc3ef72800462d9cbd5504852fe
Darshan <darshanpatel2923@gmail.com>
2025-03-06 10:30:41 +0530

    Done with setting up the blabberboot project, many things to come!!!

---
+++ BlabberBoot/server/blabberboot/src/main/java/com/blabberboot/blabberboot/BlabberbootApplication.java
@@ -0,0 +1,13 @@
+package com.blabberboot.blabberboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlabberbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlabberbootApplication.class, args);
+	}
+
+}
