32c3f9553411d6842e426ba3754654342426e0dd
Darshan <darshanpatel2923@gmail.com>
2025-03-13 21:25:37 +0530

    Done with global exception handler and custom exceptions for our business logics

--- CuraSync/patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
+++ CuraSync/patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -3,13 +3,18 @@ package com.pm.patient_service.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.service.PatientService;
+
+import jakarta.validation.Valid;
 
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -26,6 +31,12 @@ public class PatientController {
     public ResponseEntity<List<PatientResponseDTO>> getPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+        return ResponseEntity.ok().body(patientResponseDTO);
     }
 
 }
