34fba1660192cea3e85933bf6f7b1756af7ee510
Darshan <darshanpatel2923@gmail.com>
2025-03-13 18:01:23 +0530

    Done with creating patient using service layer and dtos

---
+++ CuraSync/patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -0,0 +1,31 @@
+package com.pm.patient_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.service.PatientService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/patients") // http://localhost:4000
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+}
--- CuraSync/patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ CuraSync/patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.pm.patient_service.dto.PatientReponseDTO;
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
@@ -12,19 +13,24 @@ import com.pm.patient_service.repository.PatientRepository;
 @Service
 public class PatientService {
 
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
 
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
     }
 
-    public List<PatientReponseDTO> getPatients() {
+    public List<PatientResponseDTO> getPatients() {
         List<Patient> patients = patientRepository.findAll();
         // List<PatientReponseDTO>patientReponseDTOs=patients.stream().map(patient->PatientMapper.toDTO(patient)).toList();
         // //below is same
-        List<PatientReponseDTO> patientReponseDTOs = patients.stream().map(PatientMapper::toDTO).toList();
+        List<PatientResponseDTO> patientReponseDTOs = patients.stream().map(PatientMapper::toDTO).toList();
         return patientReponseDTOs;
 
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+        return PatientMapper.toDTO(newPatient);
     }
 
 }
