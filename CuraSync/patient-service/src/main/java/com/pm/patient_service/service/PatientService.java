a0259d96eac7d713e527157f9a57ed55e4090d59
Darshan <darshanpatel2923@gmail.com>
2025-03-20 16:56:15 +0530

    Done with creating patient-service client for billing-service grpc server

--- CuraSync/patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ CuraSync/patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.exception.EmailAlreadyExistsException;
 import com.pm.patient_service.exception.PatientNotFoundException;
+import com.pm.patient_service.grpc.BillingServiceGrpcClient;
 import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
@@ -18,9 +19,11 @@ import com.pm.patient_service.repository.PatientRepository;
 public class PatientService {
 
     private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository) {
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
     }
 
     public List<PatientResponseDTO> getPatients() {
@@ -38,6 +41,11 @@ public class PatientService {
                     "A patient with this email already exists " + patientRequestDTO.getEmail());
         }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),
+                newPatient.getName(),
+                newPatient.getEmail());
+
         return PatientMapper.toDTO(newPatient);
     }
 
