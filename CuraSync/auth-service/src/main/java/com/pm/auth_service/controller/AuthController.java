259caef6a086f1ce53ebd470ad8e16a04016529e
Darshan <darshanpatel2923@gmail.com>
2025-04-09 12:43:08 +0530

    Done with all jwt validation and protecting routes. Next up... testing

--- CuraSync/auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
+++ CuraSync/auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pm.auth_service.dto.LoginRequestDTO;
@@ -37,4 +39,16 @@ public class AuthController {
         String token = tokenOptional.get();
         return ResponseEntity.ok(new LoginResponseDTO(token));
     }
+
+    @Operation(summary = "Validate Token")
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String authHeader) {
+        // Authorization: Bearer <token>
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return authService.validateToken(authHeader.substring(7)) ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
 }
